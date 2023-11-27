fe1c158b663a00568526e3d13fd694911464022b
Lin <sunburst0203@outlook.com>
2023-11-27 18:48:14 +0800

    first commit

---
+++ Try/Try.java
@@ -0,0 +1,21 @@
+package Try;
+
+public class Try {
+    public static void main(String[] args) {
+        int[] arr = {24,69,80,57,13};
+        int temp = 0;
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4 - i; j++) {
+                if(arr[j] > arr[j+1]){
+                    temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+        for (int i = 0; i < 4; i++) {
+            System.out.println(arr[i]);
+        }
+        System.out.println("first commit");
+    }
+}
